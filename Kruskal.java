0c38b213e1301f351b9c3b44f91a0619dddce977
grifcrev <64370736+grifcrev@users.noreply.github.com>
2020-04-27 01:09:30 -0400

    Update Kruskal.java

--- Kruskal.java
+++ Kruskal.java
@@ -6,6 +6,9 @@ public class Kruskal
 	static int V = 12;
 	public static int[] parent = new int[V];
 	public static int INF = Integer.MAX_VALUE;
+	static String[] cities =
+	{ "Seattle", "San Francisco", "Los Angeles", "Las Vegas", "Denver", "Minneapolis", "Dallas", "Chicago",
+			"Washington DC", "Miami", "New York", "Boston" };
 
 	public static int find(int i)
 	{
@@ -26,23 +29,33 @@ public class Kruskal
 		int mincost = 0;
 		for (int i = 0; i < V; i++)
 			parent[i] = i;
-		
+
 		int edge_count = 0;
-		
-		while(edge_count < V - 1) {
+
+		while (edge_count < V - 1)
+		{
 			int min = INF, x = -1, y = -1;
-			for(int i =0; i<cost.length; i++) {
-			for(int j = 0; j < cost[i].length; j++) {
-				if(find(i) != find(j) && cost[i][j] < min) {
-					min = cost[i][j];
-					x=i;
-					y=j;
+			for (int i = 0; i < cost.length; i++)
+			{
+				for (int j = 0; j < cost[i].length; j++)
+				{
+					if (find(i) != find(j) && cost[i][j] < min)
+					{
+						min = cost[i][j];
+						x = i;
+						y = j;
+
+					}
 				}
+
 			}
-		}
+			int totalcost = min * 1000;
 			form(x, y);
-			System.out.printf("Edge %d:(%d,%d) cost:%d \n",edge_count++, x, y, min);
-			mincost += edge_count;
+			System.out.printf(cities[x] + "->" + cities[y] + " cost: $" + totalcost + "\n", edge_count++, cities[x],
+					cities[y], totalcost);
+			mincost += totalcost;
 		}
+		int finalcost = mincost;
+		System.out.println("Total Cost: $" + finalcost);
 	}
 }
